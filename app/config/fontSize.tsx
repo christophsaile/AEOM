2eda1bcbe39b4e255ccc14c2991c812a1393328c
Christoph Saile <christoph.saile@googlemail.com>
2021-04-19 16:08:01 +0200

    feat: add rich text styles

---
+++ app/config/fontSize.tsx
@@ -0,0 +1,8 @@
+export default {
+  h1: 200,
+  h2: 100,
+  h3: 50,
+  h4: 20,
+  text: 10,
+  quote: 10,
+};
--- app/helpers/contentfulRichTextToReactNative.js
+++ app/helpers/contentfulRichTextToReactNative.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { BLOCKS, INLINES, MARKS } from '@contentful/rich-text-types';
 
+import fontSize from '../config/fontSize';
+
 export const contentfulRichTextToReactNative = {
   renderMark: {
     [MARKS.UNDERLINE]: (text) => {
-      return text;
+      return <Text style={{ textDecorationLine: 'underline' }}>{text}</Text>;
     },
     [MARKS.BOLD]: (text) => {
       return <Text style={{ fontWeight: 'bold' }}>{text}</Text>;
@@ -30,31 +32,49 @@ export const contentfulRichTextToReactNative = {
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
       return null;
     },
-    [BLOCKS.HEADING_1]: (_node, children) => <Text>{children}</Text>,
-    [BLOCKS.HEADING_2]: (_node, children) => <Text>{children}</Text>,
-    [BLOCKS.HEADING_3]: (_node, children) => <Text>{children}</Text>,
-    [BLOCKS.HEADING_4]: (_node, children) => <Text>{children}</Text>,
-    [BLOCKS.HEADING_5]: (_node, children) => <Text>{children}</Text>,
-    [BLOCKS.HEADING_6]: (_node, children) => <Text>{children}</Text>,
+    [BLOCKS.HEADING_1]: (_node, children) => (
+      <Text style={{ fontSize: fontSize.h1 }}>{children}</Text>
+    ),
+    [BLOCKS.HEADING_2]: (_node, children) => (
+      <Text style={{ fontSize: fontSize.h2 }}>{children}</Text>
+    ),
+    [BLOCKS.HEADING_3]: (_node, children) => (
+      <Text style={{ fontSize: fontSize.h3 }}>{children}</Text>
+    ),
+    [BLOCKS.HEADING_4]: (_node, children) => (
+      <Text style={{ fontSize: fontSize.h4 }}>{children}</Text>
+    ),
+    [BLOCKS.HEADING_5]: (_node, children) => (
+      <Text style={{ fontSize: fontSize.h4 }}>{children}</Text>
+    ),
+    [BLOCKS.HEADING_6]: (_node, children) => (
+      <Text style={{ fontSize: fontSize.h4 }}>{children}</Text>
+    ),
     [BLOCKS.UL_LIST]: (_node, children) => {
-      return (
-        <View>
-          {children.map((child, i) => {
-            return child;
-          })}
-        </View>
-      );
+      return children.map((child, i) => {
+        return (
+          <View style={{ display: 'flex', flexDirection: 'row' }}>
+            <Text>{'\u2022'}</Text>
+            <Text style={{ flex: 1, paddingLeft: 5 }}>{child}</Text>
+          </View>
+        );
+      });
     },
     [BLOCKS.OL_LIST]: (_node, children) => {
       return children.map((child, i) => {
-        return child;
+        return (
+          <View style={{ display: 'flex', flexDirection: 'row' }}>
+            <Text>{i + 1 + '.'}</Text>
+            <Text style={{ flex: 1, paddingLeft: 5 }}>{child}</Text>
+          </View>
+        );
       });
     },
     [BLOCKS.LIST_ITEM]: (_node, child) => {
       return <View>{child}</View>;
     },
     [BLOCKS.QUOTE]: (_node, child) => {
-      return <Text>{child}</Text>;
+      return <Text style={{ fontSize: fontSize.quote }}>{child}</Text>;
     },
     [BLOCKS.HR]: (_node, child) => {
       return <Text>{child}</Text>;
